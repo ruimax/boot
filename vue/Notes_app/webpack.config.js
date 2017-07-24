0b743c7581dcdb0d002980db4e76fd5d639cd849
ruimax <811897569@qq.com>
2017-07-24 10:36:16 +0800

    test

---
+++ vue/Notes_app/webpack.config.js
@@ -0,0 +1,21 @@
+module.export = {
+    entry: './main.js',
+    output: {
+        path: __dirname,
+        filename: 'build.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loader: 'babel',
+            exclude: /node_modules/
+        },{
+            test: /\.vue$/,
+            loader: 'vue'
+        }]
+    },
+    babel: {
+        presets: ['es2015'],
+        plugins: ['transform-runtime']
+    }
+}
